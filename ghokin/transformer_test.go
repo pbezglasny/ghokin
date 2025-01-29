27d6b3bd18603b0ac5de70cb1fe5731c398390d5
Pavel Bezglasnyy <pavelb23@gmail.com>
2025-01-29 20:43:56 +0100

    Added support of multiline scenario description

--- ghokin/transformer_test.go
+++ ghokin/transformer_test.go
@@ -459,6 +459,10 @@ func TestTransform(t *testing.T) {
 			"fixtures/comment-in-a-midst-of-row.feature",
 			"fixtures/comment-in-a-midst-of-row.feature",
 		},
+		{
+			"fixtures/multiline-scenario-description.feature",
+			"fixtures/multiline-scenario-description.feature",
+		},
 	}
 
 	for _, scenario := range scenarios {
